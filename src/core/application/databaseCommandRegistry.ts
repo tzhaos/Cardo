511fc92dbdb8ea60fe1073a975df112d63e4bc32
Khaos Tian <240478317@qq.com>
2026-07-11 23:25:05 +0800

    refactor: type database command registry by command

--- src/core/application/databaseCommandRegistry.ts
+++ src/core/application/databaseCommandRegistry.ts
@@ -1,72 +1,61 @@
 import type { WorkspaceCommand, WorkspaceCommandType } from '../contracts/workspaceCommands';
 import type { DatabaseCommandMutation, DatabaseTransaction } from './commandTypes';
-import { executePageCommand, type PageCommand } from './pageCommandHandlers';
-import { executeBoxCommand, type BoxCommand } from './boxCommandHandlers';
-import { executeItemCommand, type ItemCommand } from './itemCommandHandlers';
-import { executePreferencesCommand, type PreferencesCommand } from './preferencesCommandHandlers';
-import { executeWorkspaceImport, type WorkspaceImportCommand } from './workspaceCommandHandlers';
+import { executePageCommand } from './pageCommandHandlers';
+import { executeBoxCommand } from './boxCommandHandlers';
+import { executeItemCommand } from './itemCommandHandlers';
+import { executePreferencesCommand } from './preferencesCommandHandlers';
+import { executeWorkspaceImport } from './workspaceCommandHandlers';
 
-export type DatabaseCommandHandler = (
+export type DatabaseCommandHandler<Type extends WorkspaceCommandType> = (
   transaction: DatabaseTransaction,
-  command: WorkspaceCommand,
+  command: Extract<WorkspaceCommand, { type: Type }>,
 ) => Promise<DatabaseCommandMutation>;
 
-const pageHandler: DatabaseCommandHandler = (transaction, command) =>
-  executePageCommand(transaction, command as PageCommand);
-
-const boxHandler: DatabaseCommandHandler = (transaction, command) =>
-  executeBoxCommand(transaction, command as BoxCommand);
-
-const itemHandler: DatabaseCommandHandler = (transaction, command) =>
-  executeItemCommand(transaction, command as ItemCommand);
-
-const preferencesHandler: DatabaseCommandHandler = (transaction, command) =>
-  executePreferencesCommand(transaction, command as PreferencesCommand);
-
-const workspaceImportHandler: DatabaseCommandHandler = (transaction, command) =>
-  executeWorkspaceImport(transaction, command as WorkspaceImportCommand);
+type DatabaseCommandRegistry = {
+  [Type in WorkspaceCommandType]: DatabaseCommandHandler<Type>;
+};
 
 export const databaseCommandRegistry = Object.freeze({
-  'workspace.import': workspaceImportHandler,
-  'page.create': pageHandler,
-  'page.rename': pageHandler,
-  'page.delete': pageHandler,
-  'page.reorder': pageHandler,
-  'page.setDefault': pageHandler,
-  'page.open': pageHandler,
-  'preferences.setLocale': preferencesHandler,
-  'preferences.setColorMode': preferencesHandler,
-  'preferences.setTheme': preferencesHandler,
-  'preferences.setSearchEngine': preferencesHandler,
-  'preferences.setCustomSearchTemplate': preferencesHandler,
-  'box.create': boxHandler,
-  'box.updateFrame': boxHandler,
-  'box.rename': boxHandler,
-  'box.promote': boxHandler,
-  'box.setDetailMode': boxHandler,
-  'box.setLocked': boxHandler,
-  'box.setAppearance': boxHandler,
-  'box.setViewMode': boxHandler,
-  'box.moveToPage': boxHandler,
-  'box.collect': boxHandler,
-  'box.removeFromCollection': boxHandler,
-  'box.delete': boxHandler,
-  'collection.updateBoxFrame': boxHandler,
-  'collection.updateView': boxHandler,
-  'collection.arrange': boxHandler,
-  'canvas.arrange': boxHandler,
-  'system.constrainFrames': boxHandler,
-  'item.paste': itemHandler,
-  'item.create': itemHandler,
-  'item.rename': itemHandler,
-  'item.editContent': itemHandler,
-  'item.setPinned': itemHandler,
-  'item.reorder': itemHandler,
-  'item.moveBetweenBoxes': itemHandler,
-  'item.delete': itemHandler,
-  'bookmark.setFavicon': itemHandler,
-} satisfies Record<WorkspaceCommandType, DatabaseCommandHandler>);
+  'workspace.import': executeWorkspaceImport,
+  'page.create': executePageCommand,
+  'page.rename': executePageCommand,
+  'page.delete': executePageCommand,
+  'page.reorder': executePageCommand,
+  'page.setDefault': executePageCommand,
+  'page.open': executePageCommand,
+  'preferences.setLocale': executePreferencesCommand,
+  'preferences.setColorMode': executePreferencesCommand,
+  'preferences.setTheme': executePreferencesCommand,
+  'preferences.setSearchEngine': executePreferencesCommand,
+  'preferences.setCustomSearchTemplate': executePreferencesCommand,
+  'box.create': executeBoxCommand,
+  'box.updateFrame': executeBoxCommand,
+  'box.rename': executeBoxCommand,
+  'box.promote': executeBoxCommand,
+  'box.setDetailMode': executeBoxCommand,
+  'box.setLocked': executeBoxCommand,
+  'box.setAppearance': executeBoxCommand,
+  'box.setViewMode': executeBoxCommand,
+  'box.moveToPage': executeBoxCommand,
+  'box.collect': executeBoxCommand,
+  'box.removeFromCollection': executeBoxCommand,
+  'box.delete': executeBoxCommand,
+  'collection.updateBoxFrame': executeBoxCommand,
+  'collection.updateView': executeBoxCommand,
+  'collection.arrange': executeBoxCommand,
+  'canvas.arrange': executeBoxCommand,
+  'system.constrainFrames': executeBoxCommand,
+  'item.paste': executeItemCommand,
+  'item.create': executeItemCommand,
+  'item.rename': executeItemCommand,
+  'item.editContent': executeItemCommand,
+  'item.setPinned': executeItemCommand,
+  'item.reorder': executeItemCommand,
+  'item.moveBetweenBoxes': executeItemCommand,
+  'item.delete': executeItemCommand,
+  'bookmark.setFavicon': executeItemCommand,
+} satisfies DatabaseCommandRegistry);
 
-export function getDatabaseCommandHandler(type: WorkspaceCommandType) {
-  return databaseCommandRegistry[type];
+export function getDatabaseCommandHandler<Type extends WorkspaceCommandType>(type: Type) {
+  return databaseCommandRegistry[type] as DatabaseCommandHandler<Type>;
 }
